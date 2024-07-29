b3dad75d4a8b0627accf0ef02c4119219d4e65ce
PMarcelisROCMN <162563162+PMarcelisROCMN@users.noreply.github.com>
2024-07-29 12:03:46 +0200

    Update db.php

--- v1/controller/db.php
+++ v1/controller/db.php
@@ -7,7 +7,7 @@ class DB {
     
     public static function connectWriteDB() {
         if (self::$writeDBConnection === null) {
-            self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=tasksdb;charset=utf8', 'petermarcelisrocmn', 'petermarcelisrocmn');
+            self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=tasksdb;charset=utf8', 'petermarcelisrocmn', 'rocmnpetermarcelis');
             self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
